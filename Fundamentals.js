49cd4b4ab6a34b828586721d70e5c7fc80ebf8fe
rajatagrawl <rajat1611@gmail.com>
2024-03-19 19:54:28 +0530

    Practice if..else

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,6 +1,18 @@
 // Conditionals
-let age = 19;
+// let age = 19;
 
-if (age >=18) {
-    console.log("You are allowed to vote");
+// if (age >=18) {
+//     console.log("You are allowed to vote");
+// }
+
+let signal = "Yellow"
+
+if (signal == "Red") {
+    console.log("Stop");
+} else if (signal == "Yellow") {
+    console.log("Waiting");
+} else if (signal == "Green") {
+    console.log("Go Go Go");
+} else {
+    console.log("Invalid");
 }
