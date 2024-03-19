38e2f2f389c4992a21400f336a88784d8b45e052
rajatagrawl <rajat1611@gmail.com>
2024-03-19 20:18:29 +0530

    Practicd do..while

--- Fundamentals.js
+++ Fundamentals.js
@@ -40,7 +40,12 @@ let signal = "Green"
 // }
 
 let i = 0;
-while ( i < 5 ) {
+// while ( i < 5 ) {
+//     console.log(i);
+//     i++;
+// }
+
+do {
     console.log(i);
     i++;
-}
+} while(i > 5)
