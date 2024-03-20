53bd5d16b4432b2da3985218f7dbc223da899175
rajatagrawl <rajat1611@gmail.com>
2024-03-20 07:53:08 +0530

    Learn Arrays

---
+++ Arrays.js
@@ -0,0 +1,16 @@
+let arr1 = [32, 45, "Rajat", true];
+console.log(arr1);
+
+let arr2 = new Array();
+console.log(arr2);
+
+let arr3 = new Array(5);
+console.log(arr3);
+
+let arr4 = new Array("abc", "def", "ghi", "jkl");
+console.log(arr4);
+arr4[2] = "xyz"
+console.log(arr4);
+arr4[5] = "test"
+console.log(arr4);
+console.log(arr4[7]);
