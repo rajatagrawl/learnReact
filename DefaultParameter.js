3d3b30117b4c7a5418857914bd3c3cfbc318e06f
rajatagrawl <rajat1611@gmail.com>
2024-03-22 08:00:38 +0530

    Function with default parameter

---
+++ DefaultParameter.js
@@ -0,0 +1,8 @@
+function displayMessage(message = "This is default message. Will be displayed when no argument received in function call") {
+    console.log(message);
+}
+
+
+displayMessage("Printing the Argument Passed");
+
+displayMessage();
